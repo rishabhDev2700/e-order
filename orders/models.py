ccdeb6d193c0a75ae3b1ab2036523a9e8ba5cdc0
rishabhDev2700 <rishavkumar2700@gmail.com>
2022-10-24 17:49:37 +0530

    Added function to create order with all orderItems

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from accounts.models import User
 from store.models import Item
 
@@ -7,14 +8,15 @@ from store.models import Item
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_completed = models.BooleanField(default=False)
-    total = models.PositiveIntegerField()
+    total = models.PositiveIntegerField(default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
 
 class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(decimal_places=2, max_digits=5, default=0)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     objects = models.Manager()
 
--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 
 from orders.bag import Bag
+from orders.models import Order, OrderItem
 from store.models import Item
 
 
@@ -32,19 +33,40 @@ def bag_delete(request):
         bag.delete(item=item_id)
 
         bag_quantity = bag.__len__()
-        bag_total = bag.get_subtotal()
-        response = JsonResponse({"quantity": bag_quantity, "subtotal": bag_total})
+        bag_subtotal = bag.get_subtotal()
+        response = JsonResponse({"quantity": bag_quantity, "subtotal": bag_subtotal})
         return response
 
 
 def bag_update(request):
     bag = Bag(request)
     if request.POST.get("action") == "post":
-        item_id = int(request.POST.get("productid"))
-        quantity = int(request.POST.get("productqty"))
+        item_id = int(request.POST.get("item_id"))
+        quantity = int(request.POST.get("quantity"))
         bag.update(item=item_id, quantity=quantity)
 
         bag_quantity = bag.__len__()
         bag_subtotal = bag.get_subtotal()
-        response = JsonResponse({"qty": bag_quantity, "subtotal": bag_subtotal})
+        response = JsonResponse({"quantity": bag_quantity, "subtotal": bag_subtotal})
         return response
+
+
+def order_add(request):
+    bag = Bag(request)
+    if request.POST.get("action") == 'post':
+        order_id = request.POST.get('order_id')
+        user_id = request.user.id
+        bag_total = bag.get_subtotal()
+
+        if Order.objects.filter(id=order_id).exists():
+            pass
+        else:
+            order = Order.objects.create(user_id=user_id, total=bag_total)
+            order_id = order.pk
+            for item in bag:
+                OrderItem.objects.create(order_id=order_id,
+                                         item=item['item'],
+                                         price=item['price'],
+                                         quantity=item['quantity'], )
+            response = JsonResponse({"success": "some message"})
+            return response
