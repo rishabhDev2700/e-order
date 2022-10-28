14519aa18c7194fbcbd39caef709664f14f176e5
rishabhDev2700 <rishavkumar2700@gmail.com>
2022-10-28 06:06:42 +0530

    Added urls and dummy functions

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
 
+from accounts import views
+
+app_name = 'accounts'
 urlpatterns = [
+    path("register/", views.sign_up, name="sign_up"),
+    path("login/", views.sign_in, name="sign_in"),
+    path("logout/", views.logout_user, name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def sign_up(request):
+    pass
+def sign_in(request):
+    pass
+def logout_user(request):
+    pass
