4037179c4a99d15b9179498ac9be17121982776a
rishabhDev2700 <rishavkumar2700@gmail.com>
2022-11-01 09:38:30 +0530

    Added fields to usercreationform and methods for signing up signing in and logging out

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm as CreationForm
+from django import forms
 
 from accounts.models import User
 
 
 class UserCreationForm(CreationForm):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField()
+
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,44 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import logout, authenticate, login
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from accounts.forms import UserCreationForm
 
 
 # Create your views here.
 def sign_up(request):
-    pass
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created Successfully")
+            return redirect('accounts:sign_in')
+        else:
+            messages.error(request, 'Some error Occurred!! Please Try again.')
+            return render(request, 'accounts/sign_up.html')
+
+    form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'accounts/sign_up.html', context=context)
 
 
 def sign_in(request):
-    pass
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged In Successfully")
+            return redirect('store:home')
+        else:
+            messages.error(request, "Invalid Credentials")
+            return redirect('accounts:sign_in')
+    return render(request, 'accounts/sign_in.html')
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "Logged out successfully")
+    return redirect('accounts:sign_in')
